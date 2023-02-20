54bd3bd755ba81600354a8129f67655b9df81af1
Loudbook <loudbooked@gmail.com>
2023-02-20 18:43:16 -0500

    Redis

---
+++ src/main/kotlin/com/loudbook/dev/Redis.kt
@@ -0,0 +1,43 @@
+package com.loudbook.dev
+
+import com.loudbook.dev.api.GameInstance
+import com.loudbook.dev.api.ServerInfo
+import org.redisson.Redisson
+import org.redisson.api.RedissonClient
+import org.redisson.config.Config
+import java.io.FileInputStream
+import java.io.IOException
+import java.util.*
+
+class Redis {
+    val client: RedissonClient
+    private var uri: String? = null
+
+    init {
+        try {
+            FileInputStream("./config.properties").use { input ->
+                val prop = Properties()
+                prop.load(input)
+                this.uri = prop.getProperty("uri")
+            }
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+        }
+
+        val config = Config()
+        config.useSingleServer()
+            .address = uri
+        this.client = Redisson.create(config)
+    }
+
+    fun pushInstanceInfo(instance: GameInstance) {
+        this.client.getTopic("server-info").publish(
+            ServerInfo(
+                instance.id,
+                instance.gameType,
+                instance.maxPlayers,
+                instance.instanceContainer.players.size,
+                instance.maxPlayers - instance.instanceContainer.players.size)
+        )
+    }
+}
