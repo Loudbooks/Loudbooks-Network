6fca4062f4443d72682e5dd2e804215ae3ac53f6
Loudbook <loudbooked@gmail.com>
2023-02-20 18:43:50 -0500

    Main

---
+++ src/main/kotlin/com/loudbook/dev/LoudsGame.kt
@@ -0,0 +1,34 @@
+package com.loudbook.dev
+
+import net.minestom.server.MinecraftServer
+import net.minestom.server.coordinate.Pos
+import net.minestom.server.entity.Player
+import net.minestom.server.event.Event
+import net.minestom.server.event.EventNode
+import net.minestom.server.event.player.PlayerLoginEvent
+import net.minestom.server.extensions.Extension
+import net.minestom.server.instance.InstanceContainer
+import net.minestom.server.instance.block.Block
+import net.minestom.server.instance.generator.GenerationUnit
+import net.minestom.server.utils.NamespaceID
+
+import net.minestom.server.world.DimensionType
+
+
+
+
+
+class LoudsGame : Extension() {
+    override fun initialize() {
+        MinecraftServer.setBrandName("Loudbook's Minigames")
+
+        val gameNode: EventNode<Event> = EventNode.all("game")
+        val survivalGamesNode: EventNode<Event> = EventNode.all("survival-games")
+
+        val redis = Redis()
+    }
+
+    override fun terminate() {
+
+    }
+}
